e5b09b06fc623f3155013c655a195b9920889b1e
Juan Camilo Gonzalez <jcgonzalezb@gmail.com>
2022-11-08 13:51:23 -0500

    Logger implemented in AppTest class

--- src/test/java/com/globant/AppTest.java
+++ src/test/java/com/globant/AppTest.java
@@ -5,45 +5,48 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
+import java.util.logging.Logger;
+
+
 public class AppTest
 {
+    public Logger log = Logger.getLogger(String.valueOf(AppTest.class));
 
     @BeforeSuite
     public void setUp() {
-        System.out.println("Set up system properties for the browser");
-        System.out.println("Have a valid ESPN account");
+        log.info("Set up system properties for the browser");
+        log.info("Have a valid ESPN account");
     }
 
     @BeforeMethod
     public void launchBrowser() {
-        System.out.println("Launch the browser");
-        System.out.println("Open ESPN page: https://www.espnqa.com/?_adbock=true&src=com");
+        log.info("Launch the browser");
+        log.info("Open ESPN page: https://www.espnqa.com/?_adbock=true&src=com");
     }
 
     /**----- Test Case 1 ------ */
     @Test
     public void logInto (){
-        System.out.println("Log into ESPN page");
+        log.info("Log into ESPN page");
     }
 
     /**----- Test Case 2 ------ */
     @Test
     public void logOut(){
-        System.out.println("Log out of ESPN page");
+        log.info("Log into ESPN page");
+        log.info("Log out of ESPN page");
     }
 
     /**----- Test Case 3 ------ */
     @Test
     public void deactivateUser (){
-        System.out.println("Deactivate user");
+        log.info("Deactivate user");
     }
 
     @AfterMethod
     public void closeBrowser() {
-        System.out.println("Close ESPN page");
-        System.out.println("Close the browser");
+        log.info("Close ESPN page");
+        log.info("Close the browser");
     }
-
-
 
 }
